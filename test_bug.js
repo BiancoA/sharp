a072018cb286714a62f34c6ba34742500e10fc79
Andrea Bianco <andrea.bianco@truvis.ch>
2018-04-23 10:37:48 +0200

    [ADD] more tests

--- test_bug.js
+++ test_bug.js
@@ -1,22 +1,33 @@
 'use strict';
-const fs = require('fs');
+const fse = require('fs-extra');
 const sharp = require('./');
+const outputDir = 'result/';
 
 console.log('starting test:');
 console.log('----------------------------------------------');
-let outputDir = 'result/';
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
+async function test_return_promise() {
+  if (!fse.existsSync(outputDir)) {
+    fse.mkdirSync(outputDir);
+  }
+  let filenameNew = outputDir + "/newtiff.tiff"
+  return sharp('./test/fixtures/uncompressed_tiff.tiff')
+    .median(15)
+    .toFile(filenameNew)
+    .then(info => {
+      console.log("TIFF processed, trying to remove it");
+      try {
+        fse.removeSync(filenameNew);
+        console.log("Success: removed");
+      } catch (error) {
+        console.log("File not removed, reason: " + error.message);
+      }
+      return info;
+    });
 }
-let filenameNew = outputDir+"/newtiff.tiff"
-sharp('./test/fixtures/uncompressed_tiff.tiff')
-  .toFile(filenameNew)
-  .then(info => {
-    console.log("TIFF processed, trying to remove it");
-    try {
-      fs.unlinkSync(filenameNew);
-      console.log("Success: removed");
-    } catch (error) {
-      console.log("File not removed, reason: " + error.message);
-    }
-  });
+
+test_return_promise().then(info => {
+  //fse.removeSync(path.join(outputDir, "gloss_nf.tif"));
+  console.log(info);
+}, reason => {
+  console.log(reason);
+});
